e3314de39a239c9a9b62d3dc36db6ef5735968f8
99003172 <75420627+99003172@users.noreply.github.com>
2020-12-05 18:15:14 +0530

    Add files via upload

---
+++ exponential.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+double exponent()
+{
+  double x = 8.0;
+  return exp(x);
+}
+double natural_logarithm()
+{
+  double x = 8.0;
+  return log10(x);
+}
+double logarithm()
+{
+  double x = 8.0;
+  return log10(x);
+}
+int power()
+{
+  int x = 8;
+  int y = 2;
+  return  pow(x,y);
+}
+int decimal_to_bin()
+{
+  int decimal=10;
+  if (decimal== 0)
+    return 0;
+  else
+    return (decimal % 2 + 10*decimal_to_bin(decimal / 2));;
+}
+int main()
+{
+  printf("%d",decimal_to_bin());
+  return 0;
+}
+
