dae9fd5c532d5a80843d075f50243d0ab9b3ef69
99002575 <71598125+99002575@users.noreply.github.com>
2020-12-05 17:53:48 +0530

    init

--- sdlc_miniproject/3_Implementation/exponential.c
+++ sdlc_miniproject/3_Implementation/exponential.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <math.h>
-int exponent()
+double exponent()
 {
-  double x = 8.0, result;
-  result = exp(x);
-  double log_value = log(x);
-  double log10_value = log10(x);
-  return result;
+  double x = 8.0;
+  return exp(x);
 }
+double natural_logarithm()
+{
+  double x = 8.0;
+  return log10(x);
+}
+double logarithm()
+{
+  double x = 8.0;
+  return log10(x);
+}
+int power()
+{
+  int x = 8;
+  int y = 2;
+  return  pow(x,y);
+}
+int decimal_to_bin()
+{
+  int decimal=10;
+  if (decimal== 0)
+    return 0;
+  else
+    return (decimal % 2 + 10*decimal_to_bin(decimal / 2));
+}
+int main()
+{
+  printf("%d",decimal_to_bin());
+  return 0;
+}
+
